3485d0eb411962765475451cda075b300d868075
Aadhi <ajibaa@gmail.com>
2020-10-09 14:12:11 +0530

    Updates

---
+++ precache-manifest.4ec29360bc331794432817e2b9b543b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b86a73ecda6c7968feaab8fd13ae625",
+    "url": "/githubUserSearchReactJS/index.html"
+  },
+  {
+    "revision": "bf9f93d79d0c2df889d2",
+    "url": "/githubUserSearchReactJS/static/css/main.ea8f38dc.chunk.css"
+  },
+  {
+    "revision": "33562266ddd9b8cc417e",
+    "url": "/githubUserSearchReactJS/static/js/2.42b7c2e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/githubUserSearchReactJS/static/js/2.42b7c2e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf9f93d79d0c2df889d2",
+    "url": "/githubUserSearchReactJS/static/js/main.82424ce9.chunk.js"
+  },
+  {
+    "revision": "2957768becd6fc41038b",
+    "url": "/githubUserSearchReactJS/static/js/runtime-main.ff873651.js"
+  }
+]);
